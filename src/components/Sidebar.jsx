cd9b70b43dc222b58e8d33e89b642b59defc7bd0
Tejas <47889755+0xtejas@users.noreply.github.com>
2024-01-05 23:32:38 +0530

    Fix sidebar text color active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
 
   const { activeMenu, setActiveMenu } = useStateContext();
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 text-black'
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg dark:text-white text-md m-2 text-black'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:bg-light-gray m-2'
 
   return (
